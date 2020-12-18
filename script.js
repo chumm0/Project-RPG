363ac1b5b3d0e2ecc3b4329b098a915958402f45
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-18 12:15:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function resetVariables(){
   localStorage['firstname'] = 'Unknown';
   localStorage['lastname'] = '';
   localStorage['gender'] = 1;
+  alert(localStorage['firstname']);
 }
 
 function initializePage(){
