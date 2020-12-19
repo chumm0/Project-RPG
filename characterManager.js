29c4bf35e6a6122d3b05f9314a3c42ea93418b28
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 13:36:19 -0600

    Update characterManager.js

--- characterManager.js
+++ characterManager.js
@@ -1,7 +1,7 @@
 var generatedCharacters = [];
 
 function printGeneratedCharacters(){
-  var str = "";
+  var str = "Randomly Generated Characters:\n";
   for (var i = 0; i < generatedCharacters.length; i++){
     str += generatedCharacters[i].getFirstname() + " " + generatedCharacters[i].getHousename() + " - " + generatedCharacters[i].getRankTitle() + "\n";
   }
