97dbe272ebe2046dc40347dabafd203d55136513
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 16:49:32 -0600

    Update characterManager.js

--- characterManager.js
+++ characterManager.js
@@ -1,9 +1,21 @@
 var mainAdvisor;
 
 function generateMainAdvisor(){
-  var gender = generateRandomGender();
+  var gender = generateGender();
   mainAdvisor = new Character(generateFirstName(gender), generateHouseName(), gender, 0, 1, 1, 2, Rank.peasant, 0);
   printCharacter(mainAdvisor);
+}
+
+function generateCharacters(){
+  var characters = [];
+  //generate characters depending on settlement later
+  for (var i = 0; i < 5; i++){
+    var gender = generateGender();
+    var character = new Character(generateFirstName(gender), generateHouseName(), gender, 0, getRandomInt(3) + 1, getRandomInt(3) + 1, getRandomInt(3) + 1, generateRank(Rank.peasant, Rank.duke), 0)
+    characters.push(character);
+  }
+  
+  return characters;
 }
 
 function printCharacter(character){
