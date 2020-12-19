bebb447cddca289f644129fe589ebf062c3dfde3
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 00:08:11 -0600

    Update characterCreation.js

--- characterCreation.js
+++ characterCreation.js
@@ -1,3 +1,40 @@
+var skillpoints = 5;
+
+function createPlayerCharacter(){
+  var playerCharacter = new Character();
+  localStorage.setItem('playerCharacter', JSON.stringify(playerCharacter));
+}
+
+function updateFN(value){
+  var playerCharacter = JSON.parse(localStorage.getItem('playerCharacter'));
+  playerCharacter.setFirstName(value);
+  localStorage.setItem('playerCharacter', JSON.stringify(playerCharacter));
+}
+
+function updateHN(value){
+  var playerCharacter = JSON.parse(localStorage.getItem('playerCharacter'));
+  playerCharacter.setHouseName(value);
+  localStorage.setItem('playerCharacter', JSON.stringify(playerCharacter));
+}
+
+function updateCombat(value){
+  var playerCharacter = JSON.parse(localStorage.getItem('playerCharacter'));
+  playerCharacter.setCombat(value);
+  localStorage.setItem('playerCharacter', JSON.stringify(playerCharacter));
+}
+
+function updateIntelligence(value){
+  var playerCharacter = JSON.parse(localStorage.getItem('playerCharacter'));
+  playerCharacter.setIntelligence(value);
+  localStorage.setItem('playerCharacter', JSON.stringify(playerCharacter));
+}
+
+function updateSocial(value){
+  var playerCharacter = JSON.parse(localStorage.getItem('playerCharacter'));
+  playerCharacter.setSocial(value);
+  localStorage.setItem('playerCharacter', JSON.stringify(playerCharacter));
+}
+
 function addToSkill(varName, value, linkNum){
   if (value > 0){
     if (parseInt(getVariable("skillpoints")) > 0){
