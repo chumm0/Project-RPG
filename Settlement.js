c56b0ae6b4b15ab296f5b13482004b43df5cd379
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 15:13:10 -0600

    Create Settlement.js

---
+++ Settlement.js
@@ -0,0 +1,29 @@
+const SettlementSize = {
+  village: 1,
+  town: 2,
+  city: 3,
+};
+Object.freeze(SettlementSize);
+
+function Settlement(name, size){
+  this.name = name;
+  this.size = size;
+}
+
+Settlement.prototype.getName = function(){ return this.name; }
+Settlement.prototype.setName = function(value) { this.name = value; }
+
+Settlement.prototype.getSize = function(){ return this.size; }
+Settlement.prototype.setSize = function(value) { this.size = value; }
+
+Settlement.prototype.getSettlementSizeTitle = function() 
+{ 
+  switch(this.size){
+    case Rank.village:
+      return "village";
+    case Rank.town:
+      return "town";
+    case Rank.city:
+      return "city";
+  }
+}
