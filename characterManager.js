bef14adb544cd2c12aabd9fa14739493d8bc7aad
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 14:07:11 -0600

    Update characterManager.js

--- characterManager.js
+++ characterManager.js
@@ -3,4 +3,15 @@ var mainAdvisor;
 function generateMainAdvisor(){
   var gender = generateRandomGender();
   mainAdvisor = new Character(generateFirstName(gender), generateHouseName(), gender, 0, 1, 1, 2, Rank.peasant, 0);
+  printCharacter(mainAdvisor);
+}
+
+function printCharacter(character){
+  var str = 
+      character.getFirstname + " of House " + character.getLastname + " (" + character.getRankTitle() + ")]n" +
+      "Combat: " + character.getCombat() + "\n" +
+      "Intelligence: " + character.getIntelligence() + "\n" +
+      "Social: " + character.getSocial();
+  
+  alert(str);
 }
