613432bbefe40df3ddaa7c23e527beb16bf7ffe3
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 12:25:59 -0600

    Update characterCreation.js

--- characterCreation.js
+++ characterCreation.js
@@ -18,56 +18,46 @@ function updatePlayerGender(value){
   storeCharacter("player", player);
 }
 
-function addToPlayerCombat(value){
-  var player = parseCharacter("player");
-  if (value > 0){
-    if (skillpoints > 0){
-      player.setCombat(player.getCombat() + 1);
-      skillpoints--;
+function addToPlayerSkill(skillName, value){
+  if (skillpoints > 0){
+    skillpoints--;
+    switch(skillName){
+      case "combat":
+        player.setCombat(player.getCombat() + value);
+        break;
+      case "intelligence":
+        player.setIntelligence(player.getIntelligence() + value);
+        break;
+      case "social":
+        player.setSocial(player.getSocial() + value);
+        break;
     }
+    
+    updateVariableHTMLText();
   }
-  else if (value < 0){
-    if (player.getCombat() > 0){
-      player.setCombat(player.getCombat() - 1);
-      skillpoints++;
-    }
-  }
-  storeCharacter(player);
-  updateVariableHTMLText()
 }
 
-function addToPlayerIntelligence(value){
-  var player = parseCharacter("player");
-  if (value > 0){
-    if (skillpoints > 0){
-      player.setIntelligence(player.getIntelligence() + 1);
-      skillpoints--;
-    }
+function subtractfromPlayerSkill(skillName, value){
+  switch(skillName){
+    case "combat":
+      if(player.getCombat() > 0){
+        player.setCombat(player.getCombat() - value);
+        skillpoints++;
+      }
+      break;
+    case "intelligence":
+      if (player.getIntelligence() > 0){
+        player.setIntelligence(player.getIntelligence() - value);
+        skillpoints++;
+      }
+      break;
+    case "social":
+      if (player.getSocial() > 0){
+        player.setSocial(player.getSocial() - value);
+        skillpoints++;
+      }
+      break;
   }
-  else if (value < 0){
-    if (player.getIntelligence() > 0){
-      player.setIntelligence(player.getIntelligence() - 1);
-      skillpoints++;
-    }
-  }
-  storeCharacter(player);
-  updateVariableHTMLText()
-}
-
-function addToPlayerSocial(value){
-  var player = parseCharacter("player");
-  if (value > 0){
-    if (skillpoints > 0){
-      player.setSocial(player.getSocial() + 1);
-      skillpoints--;
-    }
-  }
-  else if (value < 0){
-    if (player.getSocial() > 0){
-      player.setSocial(player.getSocial() - 1);
-      skillpoints++;
-    }
-  }
-  storeCharacter(player);
-  updateVariableHTMLText()
+  
+  updateVariableHTMLText();
 }
