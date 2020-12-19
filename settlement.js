7c35d81abac49825449898bdcfa599fa38e019d3
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 16:48:57 -0600

    Update settlement.js

--- settlement.js
+++ settlement.js
@@ -10,11 +10,15 @@ function Settlement(name, size){
   this.size = size;
 }
 
+Settlement.prototype.generateImportantCharacters = function(){ this.importantCharacters = generateCharacters(this); }
+
 Settlement.prototype.getName = function(){ return this.name; }
 Settlement.prototype.setName = function(value) { this.name = value; }
 
 Settlement.prototype.getSize = function(){ return this.size; }
 Settlement.prototype.setSize = function(value) { this.size = value; }
+
+Settlement.prototype.getImportantCharacters = function(value) { this.importantCharacters = value; }
 
 Settlement.prototype.getSettlementSizeTitle = function() 
 { 
