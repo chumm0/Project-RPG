e9c77c2483a9fcde93f480cb9019b7d60693f470
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-18 20:09:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function updatePageText(){
   for (var i = 0; i < variableNames.length; i++){
     var x = document.getElementsByClassName(variableNames[i]);
     
-    for (var j = 0; j < x.length; x++)
+    for (var j = 0; j < x.length; j++)
     {
       x[j].innerHTML = getVariable(variableNames[i]);
     }
