9fc1d0d3173e4663783760d7f03ba5104c27aee3
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 00:24:41 -0600

    Update character.js

--- character.js
+++ character.js
@@ -16,23 +16,44 @@ const Rank = {
 Object.freeze(Rank);
 
 class Character{
-  constructor(firstname, housename, gender, rank, combat, intelligence, social){
+  constructor(){
+    
+  }
+  
+  constructor(firstname, housename, gender, combat, intelligence, social, rank, wealth){
     this.firstname = firstname;
     this.housename = housename;
     this.gender = gender;
-    this.rank = rank;
     
     this.combat = combat;
     this.intelligence = intelligence;
     this.social = social;
-   }
+    
+    this.rank = rank;
+    this.wealth = wealth;
+  }
   
+  setFirstName(firstname) { this.firstname = firstname; }
   getFirstName() { return this.firstname; }
+  
+  setHouseName(housename) { this.housename = housename; }
   getHouseName() { return this.housename; }
+  
+  setGender(gender) { this.gender = gender; }
   getGender() { return this.gender; }
+  
+  setCombat(combat) { this.combat = combat; }
+  getCombat() { return this.combat; }
+  
+  setIntelligence(intelligence) { this.intelligence = intelligence; }
+  getIntelligence() { return this.intelligence; }
+  
+  setSocial(social) { this.social = social; }
+  getSocial() { return this.social }
+  
+  setRank(rank) { this.rank = rank; }
   getRank() { return this.rank; }
   
-  getCombat() { return this.combat; }
-  getIntelligence() { return this.intelligence; }
-  getSocial() { return this.social }
+  getWealth(wealth) { this.wealth = wealth; }
+  getWealth() { return this.wealth; }
 }
