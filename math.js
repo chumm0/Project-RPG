551d351741524457fdb16e3eb1e976e873ed82a2
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 10:59:52 -0600

    Update math.js

--- math.js
+++ math.js
@@ -1,3 +1,13 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+
+function parseCharacter(varName){
+  JSON.parse(localStorage.getItem('player'));
+  return player = new Character(playerDeserialized.firstname, playerDeserialized.housename, playerDeserialized.gender, playerDeserialized.combat, playerDeserialized.intelligence, playerDeserialized.rank, playerDeserialized.wealth);
+}
+
+function storeCharacter(varObj){
+  var str = "&quot;" + varObje + "&quot;"
+  localStorage.setItem(str, JSON.stringify(varObj));
+}
