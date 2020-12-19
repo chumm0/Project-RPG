dc5847a9ad67a819b9427b4ad246d91e2a493373
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 17:46:16 -0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -7,6 +7,17 @@ function parseCharacter(varName){
   return new Character(charDeserialized.firstname, charDeserialized.housename, charDeserialized.gender, charDeserialized.skillpoints, charDeserialized.combat, charDeserialized.intelligence, charDeserialized.social, charDeserialized.rank, charDeserialized.wealth);
 }
 
+function parseCharactersList(charactersListDeserialized){
+  var charactersList = [];
+  
+  for (var i = 0; i < charactersListDeserialized.length; i++){
+    var character = new Character(charactersListDeserialized[i].firstname, charactersListDeserialized[i].housename, charactersListDeserialized[i].gender, charactersListDeserialized[i].skillpoints, charactersListDeserialized[i].combat, charactersListDeserialized[i].intelligence, charactersListDeserialized[i].social, charactersListDeserialized[i].rank, charactersListDeserialized[i].wealth);
+    charactersList.push(character);
+  }
+  
+  return charactersList;
+}
+
 function parseSettlementsList(settlementsListDeserialized){
   var settlementsList = [];
   
