4b95385e30b3aea10a1382d0e0425f1be206bb4c
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-18 23:33:21 -0600

    Update characterManager.js

--- characterManager.js
+++ characterManager.js
@@ -1,32 +1,7 @@
 var generatedCharacters = [];
 
-function generateCharacters(){
-  for (var i = 0; i < 20; i++){
-    var newCharacter = new Character(generateRandomName(), generateRandomName(), generateRandomGender(), Rank.noble, 0, 0, 0);
-    generatedCharacters.push(newCharacter);
-  }
-}
-
 function printGeneratedCharacters(){
   for (var i = 0; i < 5; i++){
     alert(generatedCharacters[i].getFirstName() + " " + generatedCharacters[i].getHouseName() + " " + generatedCharacters[i].getRank());
   }
-}
-
-function generateRandomName(){
-  return "Bob"; //test
-}
-
-function generateRandomGender(){
-  var randomNum = getRandomInt(2) + 1;
-  if (randomNum == 1){
-    return Gender.male;
-  }
-  else{
-    return Gender.female;
-  }
-}
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
 }
