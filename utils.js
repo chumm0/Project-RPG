1a5a2201af78764da1c891b8953f85309fc5be9b
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 15:16:51 -0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -7,6 +7,18 @@ function parseCharacter(varName){
   return new Character(charDeserialized.firstname, charDeserialized.housename, charDeserialized.gender, charDeserialized.skillpoints, charDeserialized.combat, charDeserialized.intelligence, charDeserialized.social, charDeserialized.rank, charDeserialized.wealth);
 }
 
-function storeCharacter(varName, varObj){
+function parseStatesList(varName){
+  var statesListDeserialized = JSON.parse(localStorage.getItem(varName) || "[]");
+  var statesList;
+  
+  for (var i = 0; i < statesListDeserialized.length; i++){
+    var state = new State(statesList[i].name, statesList[i].settlements);
+    statesList.push(state);
+  }
+  
+  return statesList;
+}
+
+function storeVariable(varName, varObj){
   localStorage.setItem(varName, JSON.stringify(varObj));
 }
