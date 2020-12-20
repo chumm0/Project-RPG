8da684db6a67a9486124a30785086d31134fe70d
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-20 00:14:14 -0600

    Update stateManager.js

--- stateManager.js
+++ stateManager.js
@@ -30,47 +30,8 @@ function initializePoliticalWorld(){
     kingdomsList.push(kingdom);
     kingdom.directVassals = kingdomsList;
   }
-  alert("1");
+  
   storeCyclicObject("kingdomsList", kingdomsList);
-  alert("2");
-}
-
-function createStates(maxStates, tier, liege){
-  var statesList = [];
-  
-  for (var i = 0; i < maxStates; i++){
-    var stateName = generateStateName(statesList);
-    var state = new State(stateName, tier, liege);
-    var vassalStates = [];
-    
-    if (tier > StateTier.county){
-      vassalStates = createStates(maxStates, tier - 1, state);
-      state.setDirectVassalStates(vassalStates);
-    }
-    else if (tier == StateTier.county) {
-      vassalStates = createSettlements(maxStates, state);
-      state.setDirectVassalStates(vassalStates);
-    }
-    
-    statesList.push(state);
-  }
-  
-  return statesList;
-}
-
-function createSettlements(maxSettlements, liege){
-  var settlementsList = [];
-  
-  for (var i = 0; i < maxSettlements; i++){
-    var settlementName = generateSettlementName();
-    var settlementSize = getRandomInt(3) + 1;
-    var importantCharacters = generateCharacters();
-    var settlement = new Settlement(settlementName, 1, liege, settlementSize, importantCharacters);
-    
-    settlementsList.push(settlement);
-  }
-  
-  return settlementsList;
 }
 
 function printWorldPoliticalMap(){
