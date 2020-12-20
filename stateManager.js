c3d789d47953fbbae7bec866a7ee43d2fc8086fa
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-20 11:30:06 -0600

    Update stateManager.js

--- stateManager.js
+++ stateManager.js
@@ -1,50 +1,42 @@
-function initializePoliticalWorld(){
-  var kingdomsList = [];
-  
-  for (var i = 0; i < getRandomInt(7) + 3; i++){
-    var kingdom = new State("name", StateTier.kingdom, null);
-    var duchiesList = [];
-    
-    for (var j = 0; j < getRandomInt(7) + 3; j++){
-      var duchy = new State("name", StateTier.duchy, kingdom);
-      var countiesList = [];
-      
-      for (var k = 0; k < getRandomInt(7) + 3; k++){
-        var county = new State("name", StateTier.county, duchy);
-        var settlementsList = [];
-        
-        for (var z = 0; z < getRandomInt(7) + 3; z++){
-          var settlement = new State("name", StateTier.settlement, county);
-          
-          settlementsList.push(settlement);
-        }
-        
-        countiesList.push(county);
-        county.directVassals = settlementsList;
-      }
-      
-      duchiesList.push(duchy);
-      duchy.directVassals = countiesList;
-    }
-    
-    kingdomsList.push(kingdom);
-    kingdom.directVassals = duchiesList;
-  }
-  
-  storeCyclicObject("kingdomsList", kingdomsList);
+function generatePoliticalEntities(){
+  alert("in");
+  var statesList = createStates(7, StateTier.kingdom);
+  alert("out");
 }
 
-function printWorldPoliticalMap(){
-  var kingdomsList = parseStatesList("kingdomsList");
-  var str = "";
+function createStates(maxStates, tier){
+  var statesList = [];
+  var directVassals = [];
   
-  for (var kingdomNum = 0; kingdomNum < kingdomsList.length; kingdomNum++){
-    var currentKingdom = kingdomsList[kingdomNum];
-    alert("1");
-    alert(currentKingdom.directVassalStates);
-    str += currentKingdom.getName() + " Kingdom has " + currentKingdom.directVassalStates.length + " direct vassals.<br><br>";
+  if (tier > StateTier.county){
+    for (var i = 0; i < maxStates; i++){
+      var stateName = generateStateName(statesList);
+      var state = new State(stateName, tier, directVassals);
+    
+      directVassals = createStates(maxStates, tier - 1);
+      state.setDirectVassals(directVassals);
+    
+      statesList.push(state);
+    }
+  }
+  else if (tier == StateTier.county){
+    directVassals = createSettlements(maxStates);
   }
   
-  var printElement = document.getElementsByClassName("print")[0];
-  printElement.innerHTML = str;
+  return statesList;
+}
+  
+  function createSettlements(maxSettlements){
+  var settlementsList = [];
+  
+  for (var i = 0; i < maxSettlements; i++){
+    var settlementName = generateSettlementName();
+    var settlementSize = getRandomInt(3) + 1;
+    var importantCharacters = generateCharacters();
+    var settlement = new Settlement(settlementName, settlementSize, importantCharacters);
+    
+    settlementsList.push(settlement);
+  }
+  
+  return settlementsList;
 }
