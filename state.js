f7a47b5952735eb79d4d36f74252adf5f100464f
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 19:37:47 -0600

    Update state.js

--- state.js
+++ state.js
@@ -1,9 +1,24 @@
-function State(name, settlements){
+const StateTier = {
+  settlement: 1,
+  county: 2,
+  duchy: 3,
+  kingdom: 4,
+};
+Object.freeze(StateTier);
+
+function State(name, stateTier, directLiegeState){
   this.name = name;
-  this.settlements = settlements;
+  this.stateTier = stateTier;
+  this.directLiegeState = directLiegeState;
 }
 
 State.prototype.setName = function(value){ this.name = value; }
 State.prototype.getName = function(){ return this.name; }
 
-State.prototype.getSettlements = function(){ return this.settlements; }
+State.prototype.getStateTier = function() { return this.stateTier; }
+
+State.prototype.setDirectLiegeState = function(value){ this.directLiegeState = value; }
+State.prototype.getDirectLiegeState = function(){ return this.directLiegeState; }
+
+State.prototype.setDirectVassalStates = function(value){ this.directVassalStates = value; }
+State.prototype.getDirectVassalStates = function(){ return this.directVassalStates; }
