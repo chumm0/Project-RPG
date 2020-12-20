f1ec378dab186d746d85e33d241afe1550f92cd9
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 19:36:56 -0600

    Update stateManager.js

--- stateManager.js
+++ stateManager.js
@@ -1,55 +1,41 @@
-function initializeStates(){
-  var statesList = [];
-  var numOfStates = getRandomInt(7) + 3; // at least 3 but no more than 7
-  for (var stateCounter = 0; stateCounter < numOfStates; stateCounter++){ // for each state
-    var settlementsList = [];
-    var numOfSettlements = getRandomInt(5) + 2; // at least 2 but no more than 5
-    
-    for (var settlementCounter = 0; settlementCounter < numOfSettlements; settlementCounter++){ // make settlements
-      var settlementName = generateSettlementName();
-      var settlementSize = getRandomInt(3) + 1;
-      var settlementImportantCharacters = generateCharacters();
-      var settlement = new Settlement(settlementName, settlementSize, settlementImportantCharacters);
-      
-      settlementsList.push(settlement);
-    }
-    
-    var stateName = generateStateName(statesList);
-    var state = new State(stateName, settlementsList)
-    statesList.push(state);
-  }
-  
-  storeVariable("statesList", statesList);
+function initializePoliticalWorld(){
+  var maxKingdoms = getRandomInt(7) + 4;
+  var kingdomsList = createState(maxKingdoms, StateTier.kingdom, null);
 }
 
-function printStates(){
-  var statesList = parseStatesList("statesList");
-  var printElement = document.getElementsByClassName("print")[0];
-  var str = "";
+function createState(maxStates, tier, liege){
+  var statesList = [];
   
-  for (var i = 0; i < statesList.length; i++){
-    var currentState = statesList[i];
-    var settlementsList = parseSettlementsList(currentState.getSettlements());
+  for (var i = 0; i < maxStates; i++){
+    var stateName = generateStateName();
+    var state = new State(stateName, tier, liege);
     
-    str += "<b>" + currentState.getName() + " has " + settlementsList.length + " settlements! Here are the settlements:</b><br>";
-    
-    for (var j = 0; j < settlementsList.length; j++){
-      var currentSettlement = settlementsList[j];
-      var importantCharactersList = parseCharactersList(currentSettlement.getImportantCharacters());
-      
-      str += currentSettlement.getName() + " - Here is a list of its important characters:<br><br>";
-      
-      for (var k = 0; k < importantCharactersList.length; k++){
-        var currentImportantCharacter = importantCharactersList[k];
-        
-        str += currentImportantCharacter.getFirstname() + " " + currentImportantCharacter.getHousename() + "<br>";
+    var vassals = [];
+    if (tier > 2){
+      for (var j = 0; j < maxStates; j++){
+        var vassal = createState(maxStates, tier - 1, state);
+        vassals.push(vassal);
       }
       
-      str += "<br>"
+      state.setDirectVassals(vassals);
     }
-    
-    str += "<br><br>";
+    else if (tier == 1){
+      createSettlements(maxStates, state);
+    }
   }
   
-  printElement.innerHTML = str;
+  return statesList;
+}
+
+function createSettlements(maxSettlements, liege){
+  var settlementsList = [];
+  
+  for (var i = 0; i < maxSettlements; i++){
+    var settlementName = generateSettlementName();
+    var settlementSize = getRandomInt(3) + 1;
+    var importantCharacters = generateCharacters();
+    var settlement = new Settlement(settlementName, 1, liege, settlementSize, importantCharacters);
+    
+    settlementsList.push(settlement);
+  }
 }
