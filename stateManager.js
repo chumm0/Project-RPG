c14058557630a1df80d19dfa4fc7fcedfb15e830
chumm0 <76222205+chumm0@users.noreply.github.com>
2020-12-19 21:50:21 -0600

    Update stateManager.js

--- stateManager.js
+++ stateManager.js
@@ -1,5 +1,5 @@
 function initializePoliticalWorld(){
-  var maxStates = getRandomInt(7) + 4;
+  var maxStates = getRandomInt(2) + 1;
   var statesList = createStates(maxStates, StateTier.kingdom, null);
   var testState = new State("name", 1, null);
   alert("start");
